19296918f1919130148da04655d6cf78f918bdf6
Raphael Alampay <raphael.alampay@gmail.com>
2020-11-07 02:33:01 +0800

    draw grid

--- pyoutliercam/__main__.py
+++ pyoutliercam/__main__.py
@@ -12,23 +12,31 @@ from modules.alarm import Alarm
 parser = argparse.ArgumentParser(description="PyOutlierCam: Anomaly Detector for Cameras")
 parser.add_argument("--mode", choices=["capture-objects", "alarm"], help="Mode to run in", required=True)
 parser.add_argument("--cam", help="Camera index to open (Required if mode is alarm)", type=int, const=1, nargs='?', default=0)
-parser.add_argument("--width", help="Width of frame to process", type=int, const=1, nargs='?', default=600)
-parser.add_argument("--height", help="Height of frame to process", type=int, const=1, nargs='?', default=400)
+parser.add_argument("--width", help="Width of frame to process", type=int, const=1, nargs='?', default=680)
+parser.add_argument("--height", help="Height of frame to process", type=int, const=1, nargs='?', default=420)
+parser.add_argument("--cell-width", help="Width of cell", type=int, const=1, nargs='?', default=170)
+parser.add_argument("--cell-height", help="Height of cell", type=int, const=1, nargs='?', default=105)
 parser.add_argument("--debug", help="Debug mode on or off", type=bool, const=1, nargs='?', default=False)
+parser.add_argument("--image", help="Image file to process (Required if mode is capture-objects)")
+parser.add_argument("--object-proposal-algo", choices=["selective-search", "edge-boxes"], help="Object proposal algo to use (Required if mode is capture-objects)", const=1, nargs='?', default="selective-search")
+parser.add_argument("--output-dir", help="Output directory for object proposals (Required if mode is capture-objects)", const=1, nargs='?', default="./")
+parser.add_argument("--object-width", help="Width of the desired object to evaluate", type=int, const=1, nargs='?', default=250)
+parser.add_argument("--object-height", help="Height of the desired object to evaluate", type=int, const=1, nargs='?', default=250)
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
     mode    = args.mode
-    width   = args.width
-    height  = args.height
     debug   = args.debug
 
-    if mode == "capture-objects":
-        pass
-    elif mode == "alarm":
-        cam = args.cam
-        cmd = Alarm(cam, width, height, debug)
+    if mode == "alarm":
+        cam         = args.cam
+        width       = args.width
+        height      = args.height
+        cell_width  = args.cell_width
+        cell_height = args.cell_height
+
+        cmd = Alarm(cam=cam, width=width, height=height, cell_width=cell_width, cell_height=cell_height, debug=debug)
 
         print("Capture at camera: %d" % (cmd.cam))
         print("Frame dimensions for processing %d x %d" % (cmd.width, cmd.height))
@@ -43,5 +51,19 @@ if __name__ == '__main__':
 
         cv2.destroyAllWindows()
         cmd.clean()
+    elif mode == "capture-objects":
+        print("Not yet implemented...")
+#        image                   = args.image
+#        object_proposal_algo    = args.object_proposal_algo
+#        output_dir              = args.output_dir
+#        object_width            = args.object_width
+#        object_height           = args.object_height
+#
+#        cmd = CaptureObjects(image, object_proposal_algo, output_dir, object_width, object_height)
+#        cmd.execute()
+#
+#        if debug:
+#            cmd.display()
+
 
     print("Done.")
--- pyoutliercam/modules/alarm.py
+++ pyoutliercam/modules/alarm.py
@@ -2,26 +2,59 @@ import cv2
 import numpy as np
 
 class Alarm:
-    def __init__(self, cam=0, width=600, height=400, debug=True):
+    def __init__(self, cam=0, width=680, height=420, cell_width=170, cell_height=105, debug=True):
         self.cam    = cam
         self.width  = width
         self.height = height
         self.cap    = cv2.VideoCapture(cam)
         self.debug  = debug
 
-        self.label_main_window      = "Main"
-        self.label_processed_frame  = "Processed"
+        self.cell_width     = cell_width
+        self.cell_height    = cell_height
+
+        self.label_main_window  = "Main"
+        self.label_debug_frame  = "Debug"
+
+        self.color      = (0, 0, 255)
+        self.thickness  = 2
 
     def run(self):
         self.ret, self.frame    = self.cap.read()
         self.bw_frame           = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.processed_frame    = cv2.resize(self.bw_frame, (self.width, self.height))
+        self.processed_frame    = cv2.resize(self.frame, (self.width, self.height))
+        self.debug_frame        = self.processed_frame.copy()
+
+        # Define areas
+        x = 0
+        y = 0
+
+        self.areas  = []
+
+        while y < self.height:
+            while x < self.width:
+                start_point = (x, y)
+                end_point   = (x + self.cell_width, y + self.cell_height)
+
+                _x = x + self.cell_width
+                _y = y + self.cell_height
+
+                roi = self.processed_frame[x:_x, y:_y]
+
+                self.areas.append({ "roi": roi, "start_point": start_point, "end_point": end_point })
+
+                x += self.cell_width
+
+            y += self.cell_height
+            x = 0
 
     def display(self):
         cv2.imshow(self.label_main_window, self.frame)
 
         if self.debug:
-            cv2.imshow(self.label_processed_frame, self.processed_frame)
+            for area in self.areas:
+                self.debug_frame = cv2.rectangle(self.debug_frame, area["start_point"], area["end_point"], self.color, self.thickness)
+
+            cv2.imshow(self.label_debug_frame, self.debug_frame)
 
     def clean(self):
         self.cap.release()
